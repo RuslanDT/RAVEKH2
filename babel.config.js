f0c7ab7a4fefbdf5e3989a5a5116a2c7beaabc11
hugogaytan10 <estardus9@gmail.com>
2023-10-19 15:22:23 -0600

    Inicio de menu desplegable

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   //assets: ["./src/assets/fonts/"], // Ruta a tu carpeta de fuentes
+  plugins: [
+    'react-native-reanimated/plugin',
+  ],
+
 };
---
+++ src/views/Prendas/Prendas.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Prendas() {
+    return (
+        <div>Prendas</div>
+    )
+}
